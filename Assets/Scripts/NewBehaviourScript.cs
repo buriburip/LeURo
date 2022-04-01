c5e661610cb9395806178dd74a9e6c9c0962615f
yamadappy <yuukierubo1@gmail.com>
2022-04-01 14:43:50 +0900

    change 2nd time

--- Assets/Scripts/NewBehaviourScript.cs
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-private int a;
+private int a = 0;
 public class NewBehaviourScript : MonoBehaviour
 {
     // Start is called before the first frame update
