0d88334421fbf4aff8bfe3f61cf820db8305b42c
yamadappy <yuukierubo1@gmail.com>
2022-04-02 11:59:55 +0900

    fix Assets/Scripts/PlayerAct.cs, Assets/Scenes/GameScene.unity
    攻撃モーション２つ、キャラ画面外にでないように

--- Assets/Scripts/PlayerAct.cs
+++ Assets/Scripts/PlayerAct.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerRunIdle : MonoBehaviour
+public class PlayerAct : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -14,7 +14,7 @@ public class PlayerRunIdle : MonoBehaviour
     void Update()
     {
         Transform myTransform = this.transform;
-        
+        Debug.Log(this.transform.position);
 
         Vector3 worldPos = myTransform.position;
         Quaternion worldRot = myTransform.rotation;
@@ -24,7 +24,28 @@ public class PlayerRunIdle : MonoBehaviour
         float y = 0.0f;
         float speed = 0.01f;
 
+        //攻撃モーション中は動かない
+        if(anim.GetCurrentAnimatorStateInfo(0).IsName("Player_Attack1") || anim.GetCurrentAnimatorStateInfo(0).IsName("Player_Attack2")) 
+        {
+            anim.SetBool("is_attack1", false);
+            anim.SetBool("is_attack2", false);
+            return;
+        }
+        //攻撃モーション
+        if(Input.GetKeyDown(KeyCode.A))
+        {
+            anim.SetBool("is_attack1", true);
+            return;
+        }
+
+        if(Input.GetKeyDown(KeyCode.S))
+        {
+            anim.SetBool("is_attack2", true);
+            return;
+        }
         
+
+        //移動と向き
         if(Input.GetKey("up"))
         {
             y = 1.0f;
@@ -50,10 +71,9 @@ public class PlayerRunIdle : MonoBehaviour
             anim.SetBool("is_running",true);
             float radian = Mathf.Atan2(y,x);
             
-            worldPos.x += Mathf.Cos(radian) * speed;
-            worldPos.y += Mathf.Sin(radian) * speed;
-            
-            myTransform.position = worldPos; 
+            worldPos.x = -10 < worldPos.x + Mathf.Cos(radian) * speed && worldPos.x + Mathf.Cos(radian) * speed < 10 ? worldPos.x + Mathf.Cos(radian) * speed : worldPos.x;
+            worldPos.y = -5  < worldPos.y + Mathf.Sin(radian) * speed && worldPos.y + Mathf.Sin(radian) * speed < 5  ? worldPos.y + Mathf.Sin(radian) * speed : worldPos.y;
+            myTransform.position = worldPos;
             myTransform.rotation = worldRot;
         }
         else
